f829001b660cc059799b307b049172f419857b52
Coconop <9349239+Coconop@users.noreply.github.com>
2024-07-18 22:45:56 +0200

    Advanced telescope config. Improved tmux look

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,39 @@
+-- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
+require("telescope").setup({
+	defaults = {
+        mappings = {
+            i = {
+                -- Ctrl+u clear prompt
+                ["<C-u>"] = false
+            },
+        },
+	},
+	pickers = {
+        live_grep = {
+            file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+            additional_args = function(_)
+                return { "--hidden" }
+            end
+        },
+		find_files = {
+            file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+            hidden = true,
+			find_command = {
+                'fd',
+                '--type',
+                'f',
+                '--color=never',
+                '--hidden',
+                '--follow',
+            }
+		},
+	},
+})
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>tf', builtin.find_files, {desc = "Search Files"})
-vim.keymap.set('n', '<leader>tg', builtin.git_files, {desc = "Search git files"})
+vim.keymap.set('n', '<leader>tg', builtin.live_grep, {desc = "Live Grep"})
 vim.keymap.set('n', '<leader>td', builtin.diagnostics, {desc = "Browse diagnostics"})
-vim.keymap.set('n', '<leader>tr', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ")})
-end, {desc = "Grep term"})
+
+-- use native fzf for a boost
+require('telescope').load_extension('fzf')
+
