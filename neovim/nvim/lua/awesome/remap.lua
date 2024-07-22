1b0301e16c28acd857a5010d8ffe7709d0d5d1e0
Coconop <Coconop_has_no_m@il.com>
2024-07-22 14:27:58 +0200

    Add custom remaps

--- neovim/nvim/lua/awesome/remap.lua
+++ neovim/nvim/lua/awesome/remap.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex, {desc = "File Explorer"})
+vim.keymap.set('n', '<leader>ns', ']s', {noremap = true, silent = true}, {desc = "Next spelling erro"})
+vim.keymap.set('n', '<leader>ps', '[s', {noremap = true, silent = true})
