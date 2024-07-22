5ac8b7919638017ca7b0d73138ab792a6c8acc72
Coconop <9349239+Coconop@users.noreply.github.com>
2024-07-22 08:29:57 +0200

    Removed a colorscheme, fixed script

--- configit.sh
+++ configit.sh
@@ -6,6 +6,6 @@ git config --global core.autocrlf input
 git config --global merge.tool "nvimdiff"
 git config --global merge.conflictStyle "merge"
 
-echo -e "Setting up global git config"
-git conifg --local user.name "Coconop"
+echo -e "Setting up localgit config"
+git config --local user.name "Coconop"
 git config --local user.email "Coconop_has_no_m@il.com"
