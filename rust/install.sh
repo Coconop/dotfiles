ba32050092c5faf667574b49988089d3ae2069e5
Coconop <9349239+Coconop@users.noreply.github.com>
2024-07-22 17:30:04 +0200

    Rationalize functions in sourceme.sh

--- rust/install.sh
+++ rust/install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${SCRIPT_DIR}/../sourceme.sh
+
 # TODO Check for minimal rust version required by tools
 
 if command -v rustc >/dev/null 2>&1; then
     echo -e "Rust is installed"
     rustc --version
-else
+fi
+
+
+if ask_for_confirmation "(Re)install last version of Rust ?"; then
     echo -e "Installing Rust..."
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
@@ -13,26 +19,28 @@ fi
 # TODO just use LSP via Neovim + Mason 
 if command -v rust-analyzer >/dev/null 2>&1; then
     echo -e "rust-analyzer is installed"
-else
+fi
+
+if ask_for_confirmation "(Re)install last version of Rust-Analyzer ?"; then
     echo -e "Installing Rust-analyzer"
     curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
     # It might fail here. Checkout https://askubuntu.com/a/1387286 and https://askubuntu.com/a/1501564 for WSL
     chmod +x ~/.local/bin/rust-analyzer
 fi
 
-# TODO Update PATH to give priority to cargo/bin
-echo -e "Installing rust-written tools"
-cargo install ripgrep
-cargo install eza
-cargo install --locked bat
-cargo install --locked broot
-cargo install fd-find
-cargo install du-dust
-
-ln -snfv $(which fdfind) ~/.local/bin/fd
-# Need to be launched once to install shell scripts
-broot
-
-# Handy alias for bat
-mkdir -p ~/.local/bin
-ln -nfvs /usr/bin/batcat ~/.local/bin/bat
+if ask_for_confirmation "Painfully slowly Install blazlingly fast tools ?"; then
+    # TODO Update PATH to give priority to cargo/bin
+    echo -e "Installing rust-written tools"
+    cargo install ripgrep
+    cargo install eza
+    cargo install --locked bat
+    cargo install --locked broot
+    cargo install fd-find
+    cargo install du-dust
+    # Need some post-instlall config
+    ln -snfv $(which fdfind) ~/.local/bin/fd
+    echo -e "Please launch ${Gre}'broot'${None} to set it up"
+    # Handy alias for bat
+    mkdir -p ~/.local/bin
+    ln -nfvs /usr/bin/batcat ~/.local/bin/bat
+fi
---
+++ sourceme.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Colors for fancy output
+Red='\033[0;31m'; BRed='\033[1;31m';
+Gre='\033[0;32m'; BGre='\033[1;32m';
+Yel='\033[0;33m'; BYel='\033[1;33m';
+Blu='\033[0;34m'; BBlu='\033[1;34m';
+Mag='\033[0;35m'; BMag='\033[1;35m';
+Cya='\033[0;36m'; BCya='\033[1;36m';
+Whi='\033[0;37m'; BWhi='\033[1;37m';
+None='\033[0m' # Return to default colour
+
+if [ "$EUID" -eq 0 ]; then
+    echo -e "${Red} Do NOT run me with sudo${None}"
+    exit 1
+fi
+
+
+check_distro() {
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    echo -e "${Cya}Running on $NAME${None}"
+  elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    echo -e "${Cya}Running on $DISTRIB_DESCRIPTION${None}"
+  elif [ -f /etc/debian_version ]; then
+    echo -e "${Cya}Running on Debian $(cat /etc/debian_version)${None}"
+  elif [ -f /etc/redhat-release ]; then
+    echo -e "${Cya}Running on $(cat /etc/redhat-release)${None}"
+  else
+    echo -e "${Cya}Linux distribution not recognized${None}"
+  fi
+}
+
+detect_package_manager() {
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    case "$ID" in
+      debian|ubuntu)
+        package_manager="apt"
+        package_opts="--no-install-recommends"
+        ;;
+      rocky|centos|rhel)
+        package_manager="dnf"
+        package_opts=""
+        ;;
+      *)
+        echo -e "${Cya}Unsupported distribution: $ID${None}"
+        package_manager=""
+        package_opts=""
+        exit 1
+        ;;
+    esac
+  else
+    echo -e "${Cya}Unable to detect OS${None}"
+    package_manager=""
+  fi
+  echo -e "${Cya}Package Manager: $package_manager${None}"
+}
+
+
+ask_for_confirmation() {
+  local prompt="$1"
+  while true; do
+      read -rp "$(echo -e ${Mag}${prompt}${None}" (y/n):")" response
+      case "$response" in
+          [Yy]* ) return 0;;  # Return true (0) for yes
+          [Nn]* ) return 1;;  # Return false (1) for no
+          * ) echo -e "${Red}Please answer yes or no.${None}";;
+      esac
+  done
+}
+
+# Stop at 1st error
+set -e
