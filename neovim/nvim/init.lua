1a268e1c235b0bffc7c7e0a75d97e3899a8ad4d1
Coconop <Coconop_has_no_m@il.com>
2024-07-28 22:52:23 +0200

    Structure plugins lazy style. Add neotree and lualine

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -1,3 +1,3 @@
-require("settings")
+require("options")
 require("keymaps")
 require("config.lazy")
--- neovim/nvim/lua/keymaps.lua
+++ neovim/nvim/lua/keymaps.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>fe", vim.cmd.Ex, {desc = "File Explorer"})
 vim.keymap.set('n', '<leader>ns', ':set spell<bar>normal! ]s<CR>', {desc = "Next misspell"}, {noremap = true, silent = true})
 vim.keymap.set('n', '<leader>ps', ':set spell<bar>normal! [s<CR>', {desc = "Previous misspell"}, {noremap = true, silent = true})
 vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
