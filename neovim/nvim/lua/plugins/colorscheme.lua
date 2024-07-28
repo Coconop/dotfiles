751f06b862136e11011beb359cb73cf9a59fac8b
Coconop <9349239+Coconop@users.noreply.github.com>
2024-07-28 22:52:23 +0200

    Structure plugins lazy style. Add neotree and lualine

--- neovim/nvim/lua/plugins/colorscheme.lua
+++ neovim/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {},
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            vim.cmd[[colorscheme tokyonight]]
+        end
     },
 }
--- neovim/nvim/lua/plugins/telecsope.lua
+++ neovim/nvim/lua/plugins/telecsope.lua
@@ -6,10 +6,10 @@ return {
         opts = {},
         config = function()
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Find Files'})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = 'Live Grep'})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Find Buffers'})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Help Tags'})
         end,
     }
 }
