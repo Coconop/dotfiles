6fe89b66ac87ae4ba20b01036b79e426bd197d52
Coconop <9349239+Coconop@users.noreply.github.com>
2024-07-29 22:06:25 +0200

    Completed migration to Lazy

--- neovim/nvim/lua/keymaps.lua
+++ neovim/nvim/lua/keymaps.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
-vim.keymap.set('n', '<leader>ns', ':set spell<bar>normal! ]s<CR>', {desc = "Next misspell"}, {noremap = true, silent = true})
-vim.keymap.set('n', '<leader>ps', ':set spell<bar>normal! [s<CR>', {desc = "Previous misspell"}, {noremap = true, silent = true})
-vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
-vim.keymap.set('n', '<leader>ll', vim.cmd.bn, {desc = "Next Vim buffer"})
-vim.keymap.set('n', '<leader>hh', vim.cmd.bp, {desc = "Prev Vim buffer"})
+vim.keymap.set('n', '<leader>ns', ':set spell<bar>normal! ]s<CR>', { desc = "Next misspell" },
+    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ps', ':set spell<bar>normal! [s<CR>', { desc = "Previous misspell" },
+    { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, '<leader>yy', '"+y', { desc = 'Copy to system clipboard' })
+vim.keymap.set({ 'n', 'x' }, '<leader>pp', '"+p', { desc = 'Paste from system clipboard' })
+vim.keymap.set('n', '<leader>ll', vim.cmd.bn, { desc = "Next Vim buffer" })
+vim.keymap.set('n', '<leader>hh', vim.cmd.bp, { desc = "Prev Vim buffer" })
