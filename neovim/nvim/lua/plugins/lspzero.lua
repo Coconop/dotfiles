e10c396417ee0b9d5dc28d1e61affb713af36035
Coconop <9349239+Coconop@users.noreply.github.com>
2024-07-30 00:09:59 +0200

    Fix lua config (termdebug)

--- neovim/nvim/lua/plugins/lspzero.lua
+++ neovim/nvim/lua/plugins/lspzero.lua
@@ -1,5 +1,54 @@
 return {
     {
+        'hrsh7th/cmp-nvim-lsp'
+    },
+    {
+        'L3MON4D3/LuaSnip',
+        dependencies = {
+            'saadparwaiz1/cmp_luasnip',
+            'rafamadriz/friendly-snippets'
+        }
+    },
+    {
+        'hrsh7th/nvim-cmp',
+        config = function()
+            local cmp = require('cmp')
+            require("luasnip.loaders.from_vscode").lazy_load()
+
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                }),
+                snippet = {
+                    expand = function(args)
+                        require 'luasnip'.lsp_expand(args.body)
+                    end
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    -- { name = 'luasnip', option = { use_show_condition = false } },
+                    -- more sources
+                }, {
+                    { name = "buffer" },
+                }),
+            })
+        end
+    },
+    {
+        'neovim/nvim-lspconfig',
+    },
+    {
         'williamboman/mason.nvim',
         config = function()
             local lsp_zero = require('lsp-zero')
@@ -12,7 +61,6 @@ return {
                 vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { desc = 'Find LSP References' },
                     { buffer = bufnr })
             end)
-
             -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
             require('mason').setup({})
         end
@@ -51,22 +99,4 @@ return {
             })
         end
     },
-    { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    {
-        'hrsh7th/nvim-cmp',
-        config = function()
-            local cmp = require('cmp')
-            local cmp_select = { behavior = cmp.SelectBehavior.Select }
-            cmp.setup({
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                }),
-            })
-        end
-    },
-    { 'L3MON4D3/LuaSnip' },
 }
