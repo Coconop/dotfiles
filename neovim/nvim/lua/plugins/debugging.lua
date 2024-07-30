03a33e947ec84567eb63ec08a1be3b97d5b3d16c
Coconop <Coconop_has_no_m@il.com>
2024-07-31 00:47:45 +0200

    Finally made UI debug work (joke's on me, need a VSCode extension WITHIN Neovim -_-). Or just use rust-gdb...

--- neovim/nvim/lua/plugins/debugging.lua
+++ neovim/nvim/lua/plugins/debugging.lua
@@ -7,7 +7,9 @@ return {
             "nvim-neotest/nvim-nio"
         },
         config = function()
-            -- Attach UI on events
+            require("dapui").setup()
+
+            -- Attach Ui on Events
             local dap, dapui = require("dap"), require("dapui")
             dap.listeners.before.attach.dapui_config = function()
                 dapui.open()
@@ -22,46 +24,58 @@ return {
                 dapui.close()
             end
 
-            -- Setup debbugers
-            dap.adapters.gdb = {
-                type = "executable",
-                command = "rust-gdb",
-                args = { "-i", "dap" }
+            dap.adapters.codelldb = {
+                type = "server",
+                port = "${port}",
+                -- Automatically spawn the server and retrieve port
+                executable = {
+                    command = 'codelldb', -- Shall be in path or need absolute
+                    args = { "--port", "${port}" }
+                }
             }
-            dap.configurations.rust = {
+            dap.configurations.cpp = {
                 {
-                    name = "Launch",
-                    type = "gdb",
+                    name = "Launch file",
+                    type = "codelldb",
                     request = "launch",
                     program = function()
                         return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
                     end,
-                    cwd = "${workspaceFolder}",
-                    stopAtBeginningOfMainSubprogram = false,
+                    cwd = '${workspaceFolder}',
+                    stopOnEntry = false,
                 },
             }
+            dap.configurations.rust = {
+                {
+                    name = "Launch file",
+                    type = "codelldb",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    cwd = '${workspaceFolder}',
+                    stopOnEntry = false,
+                    sourceLanguages = { "rust" },
+                },
+            }
+            dap.configurations.c = dap.configurations.cpp
 
             -- key mappings
-            vim.keymap.set('n', '<leader>dc', function() require('dap').continue() end, { desc = "DAP Continue" })
-            vim.keymap.set('n', '<leader>do', function() require('dap').step_over() end, { desc = "DAP Step Over" })
-            vim.keymap.set('n', '<leader>dj', function() require('dap').step_into() end, { desc = "DAP Step Info" })
-            vim.keymap.set('n', '<leader>dk', function() require('dap').step_out() end, { desc = "DAP Out" })
-            vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end,
-                { desc = "DAP Toggle Brkpt" })
-            vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end, { desc = "DAP Repl Open" })
-            vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function() require('dap.ui.widgets').hover() end,
-                { desc = "UI hover" })
-            vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
-                require('dap.ui.widgets').preview()
-            end, { desc = "UI preview" })
-            vim.keymap.set('n', '<Leader>df', function()
-                local widgets = require('dap.ui.widgets')
-                widgets.centered_float(widgets.frames)
-            end, { desc = "UI Frames" })
-            vim.keymap.set('n', '<Leader>ds', function()
-                local widgets = require('dap.ui.widgets')
-                widgets.centered_float(widgets.scopes)
-            end, { desc = "UI Scopes" })
+            vim.keymap.set('n', '<leader>dc', function() require('dap').continue() end, { desc = "Continue" })
+            vim.keymap.set('n', '<leader>dn', function() require('dap').step_over() end, { desc = "Step Over" })
+            vim.keymap.set('n', '<leader>ds', function() require('dap').step_into() end, { desc = "Step Into" })
+            vim.keymap.set('n', '<leader>df', function() require('dap').step_out() end, { desc = "Step Out" })
+            vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end, { desc = "Breakpoint" })
+
+            vim.keymap.set('n', '<leader>dt', function() require('dapui').toggle() end, { desc = "UI toggle" })
+            vim.keymap.set({ 'n', 'v' }, '<leader>de', function() require('dapui').eval() end, { desc = "Eval" })
+
+            vim.fn.sign_define('DapBreakpoint', { text = '🛑', texthl = '', linehl = '', numhl = '' })
         end
+    },
+    {
+        'mrcjkb/rustaceanvim',
+        version = '^5', -- Recommended
+        lazy = false,   -- This plugin is already lazy
     },
 }
