5fafcf579d83e5033a442f5ebb17d731ccd5b723
Coconop <Coconop_has_no_m@il.com>
2024-07-31 09:22:41 +0200

    Fixed termdebug config

--- neovim/nvim/after/plugin/termdebug.lua
+++ neovim/nvim/after/plugin/termdebug.lua
@@ -1,5 +1,5 @@
 vim.cmd('packadd termdebug')
-vim.api.nvim_set_keymap('n', '<leader>tdt', ':TermDebug<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tdt', ':Termdebug<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tdb', ':Break<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tdu', ':Clear<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tdr', ':Run<CR>', { noremap = true, silent = true })
@@ -8,4 +8,5 @@ vim.api.nvim_set_keymap('n', '<leader>tdn', ':Next<CR>', { noremap = true, silen
 vim.api.nvim_set_keymap('n', '<leader>tds', ':Step<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tdf', ':Finish<CR>', { noremap = true, silent = true })
 vim.g.termdebugger = 'rust-gdb'
-vim.g.termdebugger_wide = 1
+vim.g.termdebug_wide = 163
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit Terminal Mode' }, { silent = true })
