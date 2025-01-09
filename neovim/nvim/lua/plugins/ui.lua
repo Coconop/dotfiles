0ef54212f6398b2eba5b24901a01a81e286641b8
Coconop <9349239+Coconop@users.noreply.github.com>
2025-01-09 10:53:29 +0100

    Restart LSP config from scratch

--- neovim/nvim/lua/plugins/ui.lua
+++ neovim/nvim/lua/plugins/ui.lua
@@ -1,3 +1,9 @@
 return {
-    { 'nvim-tree/nvim-web-devicons', opts = {} }
+    { 'nvim-tree/nvim-web-devicons', opts = {} },
+    {
+        'rcarriga/nvim-notify',
+        config = function ()
+           vim.notify = require("notify")
+        end
+    }
 }
