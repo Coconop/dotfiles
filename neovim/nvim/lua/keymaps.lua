e89345c405413d8e1cd9835c6693a83b0021b66c
Coconop <Coconop_has_no_m@il.com>
2025-01-09 14:17:06 +0100

    Tested cfg OK

--- neovim/nvim/lua/keymaps.lua
+++ neovim/nvim/lua/keymaps.lua
@@ -2,15 +2,13 @@
 vim.g.mapleader = " "
 
 -- Spelling
-vim.keymap.set('n', '<leader>ns', ':set spell<bar>normal! ]s<CR>', { desc = "Next misspell" },
-    { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>ps', ':set spell<bar>normal! [s<CR>', { desc = "Previous misspell" },
-    { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ns", ":set spell<bar>normal! ]s<CR>", { desc = "Next misspell" })
+vim.keymap.set("n", "<leader>ps", ":set spell<bar>normal! [s<CR>", { desc = "Previous misspell" })
 
 -- Easy System copy/paste
-vim.keymap.set({ 'n', 'x' }, '<leader>yy', '"+y', { desc = 'Copy to system clipboard' })
-vim.keymap.set({ 'n', 'x' }, '<leader>pp', '"+p', { desc = 'Paste from system clipboard' })
+vim.keymap.set({ "n", "x" }, "<leader>yy", '"+y', { desc = "Copy to system clipboard" })
+vim.keymap.set({ "n", "x" }, "<leader>pp", '"+p', { desc = "Paste from system clipboard" })
 
 -- Buffer navigation
-vim.keymap.set('n', '<leader>ll', vim.cmd.bn, { desc = "Next Vim buffer" })
-vim.keymap.set('n', '<leader>hh', vim.cmd.bp, { desc = "Prev Vim buffer" })
+vim.keymap.set("n", "<leader>ll", vim.cmd.bn, { desc = "Next Vim buffer" })
+vim.keymap.set("n", "<leader>hh", vim.cmd.bp, { desc = "Prev Vim buffer" })
