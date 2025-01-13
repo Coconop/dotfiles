4904985f759372724b989eddd16cc41163bb0ee0
Coconop <Coconop_has_no_m@il.com>
2025-01-13 11:19:33 +0100

    Keep refactorig

---
+++ bash/.bash.d/config.bash
@@ -0,0 +1,33 @@
+# I use (n)vim, BTW
+export VISUAL=nvim
+export EDITOR="$VISUAL"
+export GIT_EDITOR=nvim
+
+# Use blazingly fast and pretty tool
+alias ll='eza -alF'
+
+# Prompt for SSH credentials from Terminal, no GUI
+unset SSH_ASKPASS
+# Same for GPG
+export GPG_TTY=$(tty)
+
+# C
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# Rust
+source "${HOME}/.cargo/env"
+export RUST_SRC_PATH=$(rustc --print sysroot)/lib/rustlib/src/rust/library/
+
+# Go
+export PATH=${PATH}:/usr/local/go/bin:${HOME}/go/bin
+
+# Autocorrect bad commands
+#eval "$(thefuck --alias)"
+
+# Local folder for custom scripts/exe
+mkdir -p ${HOME}/.local/bin
+
+export PATH=${HOME}/.local/bin:${PATH}
+
+# Set up fzf key bindings and fuzzy completion
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
