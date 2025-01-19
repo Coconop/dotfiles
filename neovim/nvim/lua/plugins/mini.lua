5cfea817d53b956fae9fa32220df09acc908ae1b
Coconop <Coconop_has_no_m@il.com>
2025-01-19 14:14:34 +0100

    Fixed toggle of visual lines. Add some mini plugins :)

--- neovim/nvim/lua/plugins/mini.lua
+++ neovim/nvim/lua/plugins/mini.lua
@@ -6,6 +6,13 @@ return {
 			require("mini.ai").setup({ n_lines = 500 })
 			-- Fast and feature-rich surround actions
 			require("mini.surround").setup()
+			-- Visualize current scope
+			require("mini.indentscope").setup({
+				delay = 0,
+				animation = function(n, s) return 0 end
+			})
 		end,
 	},
+	-- Handy comments
+	{ 'echasnovski/mini.comment', version = false },
 }
--- neovim/nvim/lua/plugins/treesitter.lua
+++ neovim/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
     {
-        "nvim-treesitter/nvim-treesitter", 
+        "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function () 
+        config = function()
             local configs = require("nvim-treesitter.configs")
             configs.setup({
                 ensure_installed = { "c", "lua", "vim", "vimdoc", "rust", "bash", "json", "toml", "python" },
                 sync_install = false,
                 highlight = { enable = true },
-                indent = { enable = true },  
+                indent = { enable = true },
                 -- TODO https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#modules
                 -- Automatically install missing parsers when entering buffer (need tree-sitter CLI)
                 -- auto_install = true,
