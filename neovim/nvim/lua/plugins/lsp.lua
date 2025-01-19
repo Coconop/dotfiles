de80943a508c567487661e43d47213cca5c18462
Coconop <9349239+Coconop@users.noreply.github.com>
2025-01-19 14:14:34 +0100

    Fixed toggle of visual lines. Add some mini plugins :)

--- neovim/nvim/lua/plugins/lsp.lua
+++ neovim/nvim/lua/plugins/lsp.lua
@@ -110,23 +110,27 @@ return {
 			require("lsp_lines").setup()
 			-- Disable virtual_text since it's redundant due to lsp_lines.
 			vim.diagnostic.config({ virtual_text = false })
-			-- -- Don't underline HINTs, can be annoying with #[cfg()]
+
+			-- Don't underline HINTs, can be annoying with #[cfg()]
 			vim.diagnostic.config({
 				underline = { severity = { min = vim.diagnostic.severity.INFO } },
 			})
+
 			-- We want to be able to toggle it if its to annyoing
 			vim.keymap.set("", "<Leader>vl", require("lsp_lines").toggle, { desc = "[V]irtual [L]ines toggle" })
+
 			-- Disable for floating windows (Lazy, Mason)
 			vim.api.nvim_create_autocmd("WinEnter", {
 				callback = function()
 					local floating = vim.api.nvim_win_get_config(0).relative ~= ""
 					vim.diagnostic.config({
 						virtual_text = floating,
-						virtual_lines = not floating,
+						-- Keep it disabled by default
+						virtual_lines = false
 					})
 				end,
 			})
-			-- Disabled by default, toggle to enablv
+			-- Disable it by default, enable it via keymaps
 			vim.diagnostic.config({ virtual_lines = false })
 		end,
 	},
--- neovim/nvim/lua/plugins/oil.lua
+++ neovim/nvim/lua/plugins/oil.lua
@@ -1,8 +1,6 @@
 return {
 	{
 		"stevearc/oil.nvim",
-		---@module 'oil'
-		---@type oil.SetupOpts
 		opts = {},
 		-- Optional dependencies
 		--dependencies = { { "echasnovski/mini.icons", opts = {} } },
