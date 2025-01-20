e31831db581e0abf0859083b78f21b5813b0ee0d
Coconop <Coconop_has_no_m@il.com>
2025-01-20 15:27:56 +0100

    Keep ricing fzf. Customized UI and keybindings

--- bash/.bash.d/fzf.bash
+++ bash/.bash.d/fzf.bash
@@ -6,7 +6,21 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS="--preview 'bat --style=numbers --color=always --line-range :500 {}' --height 40% --border"
 
 
+# Print tree structure in the preview window
+export FZF_ALT_C_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'tree -C {} | head -200'"
+
+
 # Set up fzf key bindings and fuzzy completion
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
+# Search and open in neovim (support multiple file selection)
 alias nn='nvim $(fzf -m --preview="bat --color=always {}")'
+# Upgrade fzf
+alias fzfup='cd ~/git/fzf && git pull && ./install'
+
+# remap ALT-C to ALT-F (easier with homerow keyboard)
+bind -m emacs-standard '"\ef": " \C-b\C-k \C-u`__fzf_cd__`\e\C-e\er\C-m\C-y\C-h\e \C-y\ey\C-x\C-x\C-d"'
+bind -m vi-command '"\ef": "\C-z\ec\C-z"'
+bind -m vi-insert '"\ef": "\C-z\ec\C-z"'
--- neovim/nvim/lua/plugins/oil.lua
+++ neovim/nvim/lua/plugins/oil.lua
@@ -2,9 +2,7 @@ return {
 	{
 		"stevearc/oil.nvim",
 		opts = {},
-		-- Optional dependencies
-		--dependencies = { { "echasnovski/mini.icons", opts = {} } },
-		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
 	},
 }
