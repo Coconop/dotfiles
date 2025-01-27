11dac9598eecec5d28a3698d3cfc5a6456f3b67f
Coconop <Coconop_has_no_m@il.com>
2025-01-27 13:48:57 +0100

    Pimped prompt with nerdfont

--- bash/.bash.d/prompt.bash
+++ bash/.bash.d/prompt.bash
@@ -74,9 +74,9 @@ __set_bash_prompt()
 
     # Visual last status code
     if [[ $exit -eq 0 ]]; then
-        status="${Gre}•${None}"
+        status="${Gre}${None}"
     else
-        status="${Red}•${None}"
+        status="${Red}${None}"
     fi
 
     # Detect virtual env
@@ -97,9 +97,9 @@ __set_bash_prompt()
 
     # Detect SSH session
     if [[ -n "$SSH_CLIENT" || -n "$SSH_TTY" ]]; then
-        local ssh_ps1="${Yel}󰒍${clr}"
+        local ssh_ps1="${Yel}${clr}"
     else
-        local ssh_ps1="${Gre}󰌢${clr}"
+        local ssh_ps1="${Gre}${clr}"
     fi
 
     local time="\t"
