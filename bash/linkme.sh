375009620c9ef1cdee9cfc2c379911dd19f46fdf
Coconop <Coconop_has_no_m@il.com>
2025-02-03 22:22:11 +0100

    Manual Tmux Status Line config B-) NO PLUGINS! Precised shellcheck script name

--- bash/linkme.sh
+++ bash/linkme.sh
@@ -6,3 +6,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ln -sfnv ${SCRIPT_DIR}/.bashrc 	            ${HOME}/.bashrc
 ln -sfnv ${SCRIPT_DIR}/.bash.d 	            ${HOME}/.bash.d
 ln -sfnv ${SCRIPT_DIR}/.bash_local.d        ${HOME}/.bash_local.d
+touch ${HOME}/.bash_local.d/hush.bash
