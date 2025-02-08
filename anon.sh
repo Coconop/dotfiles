6eaee46ffd6ad42e46d9f802a27c7d2bb29f9ada
Coconop <9349239+Coconop@users.noreply.github.com>
2025-02-09 00:05:30 +0100

    Removed DATE update

--- anon.sh
+++ anon.sh
@@ -5,7 +5,6 @@ export GIT_AUTHOR_NAME="Coconop"
 export GIT_AUTHOR_EMAIL="9349239+Coconop@users.noreply.github.com"
 export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
-export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"
 " --tag-name-filter cat -- --all
 
 # Cleanup: Remove old refs and expire reflogs
