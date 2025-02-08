36bcb7cacf0fcde2498a459a70459cd57c09a11d
Coconop <9349239+Coconop@users.noreply.github.com>
2025-02-09 00:07:51 +0100

    WIP

--- anon.sh
+++ anon.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+# New author and committer details
+NEW_NAME="Coconop"
+NEW_EMAIL="9349239+Coconop@users.noreply.github.com"
+
+# Rewrite all commits in all branches
 git filter-branch -f --env-filter "
-export GIT_AUTHOR_NAME="Coconop"
-export GIT_AUTHOR_EMAIL="9349239+Coconop@users.noreply.github.com"
-export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
-export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+    export GIT_AUTHOR_NAME='$NEW_NAME'
+    export GIT_AUTHOR_EMAIL='$NEW_EMAIL'
+    export GIT_COMMITTER_NAME='$NEW_NAME'
+    export GIT_COMMITTER_EMAIL='$NEW_EMAIL'
 " --tag-name-filter cat -- --all
 
 # Cleanup: Remove old refs and expire reflogs
