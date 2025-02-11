a8526fa7735f2910e2039039a8373fc116d5884a
Coconop <Coconop_has_no_m@il.com>
2025-02-11 16:50:46 +0100

    Restored cscope custom functions

--- neovim/nvim/lua/plugins/c.lua
+++ neovim/nvim/lua/plugins/c.lua
@@ -17,4 +17,31 @@ return {
             change_cwd = true,
         },
     },
+    -- Build cscope.files (required to build database)
+    vim.keymap.set("n", "<leader>gl", function()
+        -- List files with fd
+        local cmd = 'fd -t f -e c -e h > cscope.files'
+        -- List files with find
+        -- local cmd = 'find . -type f \\( -name "*.c" -o -name "*.h" \\) > cscope.files'
+
+        -- Run the command
+        vim.fn.system(cmd)
+
+        -- Notify the user
+        print("cscope.files generated")
+    end, { desc = "Generate cscope.files list" }),
+
+    -- View callers in picker
+    vim.keymap.set("n", "<leader>gvi", function()
+        local func = vim.fn.expand("<cword>")
+        local command = ":CsStackView open down " .. func
+        vim.cmd(command)
+    end, { desc = "View callers in picker" }),
+
+    -- View callee in picker
+    vim.keymap.set("n", "<leader>gvo", function()
+        local func = vim.fn.expand("<cword>")
+        local command = ":CsStackView open up " .. func
+        vim.cmd(command)
+    end, { desc = "View callees in picker" }),
 }
