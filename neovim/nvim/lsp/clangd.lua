525ab4f83f0677b20983270e1a2e58396dba5f0c
Coconop <9349239+Coconop@users.noreply.github.com>
2025-06-02 11:31:51 +0200

    Add cmd to add compile_flags.txt

--- neovim/nvim/lsp/clangd.lua
+++ neovim/nvim/lsp/clangd.lua
@@ -11,9 +11,4 @@ return {
     },
     filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
     root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "compile_flags.txt", "configure.ac", ".git" },
-    init_options = {
-        usePlaceholders = true,
-        completeUnimported = true,
-        clangdFileStatus = true
-    },
 }
--- neovim/nvim/lua/plugins/lsp.lua
+++ neovim/nvim/lua/plugins/lsp.lua
@@ -6,6 +6,69 @@ now(function()
         source = "neovim/nvim-lspconfig",
     })
 
+    -- clangd specific
+    -- For Makefile projects that have no CMakeLists (compile_commands.json)
+    -- If there is one, just '-DCMAKE_EXPORT_COMPILE_COMMANDS=1'
+    local function create_compile_flags()
+
+        -- Try to automatically find where to put it
+        local function find_project_root()
+            local root_patterns = {'.git', 'Makefile' }
+            local current_dir = vim.fn.expand('%:p:h')
+
+            -- Look upwards until matching pattern or reaching '/'
+            for _, pattern in ipairs(root_patterns) do
+                local found = vim.fn.findfile(pattern, current_dir .. ';')
+                if found ~= '' then
+                    return vim.fn.fnamemodify(found, ':h')
+                end
+                found = vim.fn.finddir(pattern, current_dir .. ';')
+                if found ~= '' then
+                    return vim.fn.fnamemodify(found, ':h')
+                end
+            end
+
+            -- fallback to current working dir
+            vim.notify('No Root pattern found', vim.log.levels.WARN)
+            return vim.fn.getcwd()
+        end
+
+        local project_root = find_project_root()
+        local compile_flags_path = project_root .. '/compile_flags.txt'
+
+        -- Check if file exists
+        if vim.fn.filereadable(compile_flags_path) == 1 then
+            vim.notify('compile_flags.txt already exists !', vim.log.levels.WARN)
+            return
+        end
+
+        local default_flags = {
+            '-Wall',
+            '-Wextra',
+            '-std=gnu99',
+            '-pipe',
+            '-fexceptions',
+            '-fstack-protector-strong',
+            '-fpic',
+            '-I.',
+            '-I./include',
+            '-D_GNU_SOURCE'
+        }
+
+        -- Write the file
+        local file = io.open(compile_flags_path, "w")
+        if file then
+            for _, flag in ipairs(default_flags) do
+                file:write(flag .. '\n')
+            end
+            file:close()
+            vim.notify('compile_flags.txt created at:' .. compile_flags_path, vim.log.levels.INFO)
+            vim.notify('call gen_compile_db.sh', vim.log.levels.INFO)
+        else
+            vim.notify('Error: Could not create compile_flags.txt', vim.log.levels.ERROR)
+        end
+    end
+
     -- Global lsp config ----------------------------------------------
 
     -- Always let space for diagnostics, signs, etc
@@ -33,6 +96,13 @@ now(function()
             "document [S]ymbols")
             map("<leader>lR", vim.lsp.buf.rename, "[R]ename")
             map("<leader>lc", vim.lsp.buf.code_action, "[C]ode action", { "n", "x" })
+
+            -- clangd specific
+            local client = vim.lsp.get_client_by_id(event.data.client_id)
+            if client and client.name == 'clangd' then
+                map("<leader>lf", create_compile_flags,
+                    "Create [f]ile compile_flags.txt")
+            end
         end,
     })
 
