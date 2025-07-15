cbad18ffb46b3e17bbed0fae12d914e22bf32626
Coconop <9349239+Coconop@users.noreply.github.com>
2025-07-15 10:28:28 +0200

    chore: Add comment to config pass for local repo. Put python script away in its folder.

--- git/git_config.sh
+++ git/git_config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source ${SCRIPT_DIR}/../sourceme.sh
+source "${SCRIPT_DIR}"/../sourceme.sh
 
-safe_symlink ${SCRIPT_DIR}/.gitignore_global ${HOME}/.gitignore_global
+safe_symlink ${SCRIPT_DIR}/.gitignore_global "${HOME}"/.gitignore_global
 
 echo -e "Setting up global git config"
 git config --global core.editor "nvim"
 git config --global core.autocrlf input
 git config --global merge.tool "nvimdiff"
 git config --global merge.conflictStyle "merge"
-git config --global core.excludesFile ${HOME}/.gitignore_global
+git config --global core.excludesFile "${HOME}/".gitignore_global
 
 echo -e "Setting up local git config"
 # Hey that's me :)
@@ -21,3 +21,6 @@ git config --local user.email "9349239+Coconop@users.noreply.github.com"
 git config --local commit.gpgsign false
 # We don't care about line endings
 git config --local core.safecrlf false
+
+ git congif --local credential.username "coconop"
+# git congif --local credential.helper "!func(){ echo "username=coconop"; echo "password=$(pass git/github/coconop)"; }; func"
