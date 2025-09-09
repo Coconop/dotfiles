9d50acf9a52072c79aae497d78972fa38c71cbc3
Coconop <9349239+Coconop@users.noreply.github.com>
2025-09-09 11:27:06 +0200

    chores: Add WIP function

--- clang/show_includes.sh
+++ clang/show_includes.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 # Extract all directories containing included headers
 
+# TODO
+progress-bar() {
+	local current=$1
+	local len=$2
+
+	local bar_char='|'
+	local empty_char=' '
+	local length=50
+	local perc_done=$((current * 100 / len))
+	local num_bars=$((perc_done * length / 100))
+
+	local i
+	local s='['
+	for ((i = 0; i < num_bars; i++)); do
+		s+=$bar_char
+	done
+	for ((i = num_bars; i < length; i++)); do
+		s+=$empty_char
+	done
+	s+=']'
+
+	echo -ne "$s $current/$len ($perc_done%)\r"
+}
+
 # Find all C/C++ source files
 src_files=$(find . -type f \( -name "*.c" -o -name "*.cpp" -o -name "*.h" \))
 
