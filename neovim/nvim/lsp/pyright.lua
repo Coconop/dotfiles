9c3d8feb9dc2b4f96b43fe871c8b25cb9341e2eb
Coconop <9349239+Coconop@users.noreply.github.com>
2026-02-20 13:23:56 +0100

    chores(LSP): Remove recursive search. Todo: config path via private file or .env

--- neovim/nvim/lsp/pyright.lua
+++ neovim/nvim/lsp/pyright.lua
@@ -1,26 +1,3 @@
--- Search for a virtual env recursively from HOME
-local function find_venv()
-    local venv = vim.fs.find(
-        -- Python virtual env contain pyvenv.cfg on both windows and linux
-        { "pyvenv.cfg" },
-        {
-            path = vim.fn.expand('~'),
-        }
-    )[1]
-
-    if venv then
-        return vim.fs.dirname(venv)
-    end
-end
-
-local venv = find_venv()
-local extra_src = "."
-if venv then
-    extra_src = vim.fs.find(
-        {"gofer"},
-        {path=venv})
-end
-
 return {
     cmd = { "pyright-langserver", "--stdio" },
     filetypes = { "python" },
@@ -32,23 +9,6 @@ return {
         ".git",
     },
 
-    before_init = function(_, config)
-
-        if venv then
-            config.settings = config.settings or {}
-            config.settings.python = config.settings.python or {}
-
-            config.settings.python.pythonPath =
-                vim.fs.joinpath(venv, "bin", "python") -- linux/mac
-
-
-            if vim.loop.os_uname().sysname == "Windows_NT" then
-                config.settings.python.pythonPath =
-                    vim.fs.joinpath(venv, "Scripts", "python.exe")
-            end
-        end
-    end,
-
     settings = {
         python = {
             analysis = {
@@ -56,7 +16,10 @@ return {
                 useLibraryCodeForTypes = true,
                 diagnosticMode = "workspace",
                 typeCheckingMode = "standard",
-                extraPaths = extra_src,
+                venvPath = vim.fs.normalize(vim.fn.expand('~')),
+                venv = "py312",
+                extraPaths = {vim.fs.joinpath(vim.fn.expand("~"), "gofer", "gofer")},
+                logLevel = "Trace"
             },
         },
     },
