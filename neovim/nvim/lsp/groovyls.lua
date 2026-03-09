3df594d12a55e434fa49196a7ab723bcf8f54cb9
Coconop <9349239+Coconop@users.noreply.github.com>
2026-03-09 17:11:39 +0100

    chores(lsp): add groovy root markers

--- neovim/nvim/lsp/groovyls.lua
+++ neovim/nvim/lsp/groovyls.lua
@@ -2,4 +2,5 @@ return {
     local jar_path = vim.fn.glob(vim.fn.expand("~/git/groovy-language-server/build/libs/*all.jar"))
     cmd = { "java", "-jar" },
     filetypes = { "groovy" },
+    root_markers = { "Jenkinsfile", ".git"}
 }
