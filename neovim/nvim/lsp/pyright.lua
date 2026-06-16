d75bf8f36633b79b9dc37578e1fc065d6ef99b09
Coconop <9349239+Coconop@users.noreply.github.com>
2026-06-16 11:50:45 +0200

    chores(pyright): Generic pyright cfg: relies on custom cfg by project

--- neovim/nvim/lsp/pyright.lua
+++ neovim/nvim/lsp/pyright.lua
@@ -1,3 +1,26 @@
+-- Look for custom config file on which virtual env to use
+local function find_pyright_cfg()
+  local dir = vim.fn.expand("%:p:h")
+  local home = vim.fn.expand("~")
+
+  while true do
+    local cfg_path = dir .. "/.pyright_cfg.lua"
+    if vim.fn.filereadable(cfg_path) == 1 then
+      vim.notify("Found config: " .. cfg_path .. "]")
+      return dofile(cfg_path) -- found: execute and return the config table
+    end
+    if dir == home then break end -- Do not walk up past HOME
+    local parent = vim.fn.fnamemodify(dir, ":h")
+    if parent == dir then break end  -- Do not walk up past ROOT
+    dir = parent
+  end
+
+  return nil -- no config found :(
+end
+
+local cfg = find_pyright_cfg() or {}
+vim.notify("[" .. cfg.venv .. "]" .. vim.inspect(cfg.extraPaths))
+
 return {
     cmd = { "pyright-langserver", "--stdio" },
     filetypes = { "python" },
@@ -16,9 +39,9 @@ return {
                 useLibraryCodeForTypes = true,
                 diagnosticMode = "workspace",
                 typeCheckingMode = "standard",
-                venvPath = vim.fs.normalize(vim.fn.expand('~')),
-                venv = "py312",
-                extraPaths = {vim.fs.joinpath(vim.fn.expand("~"), "gofer", "gofer")},
+                venvPath = cfg.venvPath,
+                venv = cfg.venv,
+                extraPaths = cfg.extraPaths,
                 logLevel = "Trace"
             },
         },
